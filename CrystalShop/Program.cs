30e2ec94519f21f00d1ace1db295bb26695eefd2
Антон Красноперов <an2niy85@mail.ru>
2021-11-17 22:22:14 +0400

    Добавьте файлы проекта.

---
+++ CrystalShop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystalShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountGold;
+            int amountCrystal;
+            int costCrystal = 7;
+            
+            Console.WriteLine("Сколько у Вас есть золота?");
+            amountGold = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Один кристалл стоит {costCrystal} золота.");
+            Console.WriteLine("Какое кол-во кристалов желаете приобрести?");
+            amountCrystal = Convert.ToInt32(Console.ReadLine());
+
+            amountGold -= amountCrystal * costCrystal;
+
+            Console.WriteLine("У вас - " + amountCrystal + " кристалов и " + amountGold + " золота.");
+            Console.ReadLine();
+        }
+    }
+}
